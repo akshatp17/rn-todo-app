d9acc0dc831306818c9ec071131b4333460279a2
Akshat Pratyush <akshatp0505@gmail.com>
2025-03-16 00:16:05 +0530

    Project setup

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-4xl text-center">
+        Welcome! This is the home page fot todo app
+      </Text>
     </View>
   );
 }
